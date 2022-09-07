a786f02be98315730a561a524e059911b2af742d
UdaraKammanankada <udarakaushalya55@gmail.com>
2022-09-07 09:18:29 +0530

    udara

--- src/test/java/API.java
+++ src/test/java/API.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,15 +10,21 @@ public class API {
         RestAssured.baseURI = "https://reqres.in";
 
 
-        String response2 =   given().log().all().get("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
+        String response1 =   given().log().all().get("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
+        System.out.println(response1);
+
+        String response2 =   given().log().all().body(BodyJson.patch()).patch("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
         System.out.println(response2);
 
-        String response3 =   given().log().all().body(BodyJson.patch()).patch("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
+
+        String response3 =   given().log().all().get("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
         System.out.println(response3);
 
-
-        String response4 =   given().log().all().get("/api/users/2").then().assertThat().statusCode(200).extract().response().asString();
-        System.out.println(response4);
+        JsonPath jsl = new JsonPath(response3);
+        String firstname = jsl.getString("data.first_name");
+        System.out.println(firstname);
+        String updatedfirstname = "john";
+        Assert.assertEquals(firstname,updatedfirstname);
 
     }
 
--- src/test/java/BodyJson.java
+++ src/test/java/BodyJson.java
@@ -1,18 +1,12 @@
 public class BodyJson {
 
-    public static String post()
-    {
-        return "{\n" +
-                "    \"name\": \"udara\",\n" +
-                "    \"job\": \"leader\"\n" +
-                "}";
-    }
+
 
     public static String patch()
     {
         return "{\n" +
-                "    \"name\": \"morpheus\",\n" +
-                "    \"job\": \"zion resident\"\n" +
+                "    \"first_name\": \"john\",\n" +
+                "    \"last_name\": \"doe\"\n" +
                 "}";
     }
 
